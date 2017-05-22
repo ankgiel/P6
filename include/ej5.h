570e2a9261e6c542d15bc2ba6e2ec923f731a88e
iscoct <iscoct@correo.ugr.es>
2017-05-22 23:25:11 +0200

    Update ej5.h

--- include/ej5.h
+++ include/ej5.h
@@ -1,10 +1,11 @@
 #ifndef EJ_5
 #define EJ_5
+#include <iostream>
+using namespace std;
 
 typedef double TValor
 
 int BuscaMax(int * vec, int );
-int numElementos(TValor * valores); 
 
 class MatrizDispersa{
 private:
@@ -13,9 +14,12 @@ private:
 	Valor * valores;
 	int numeroValores;
 
+	int numElementos(TValor * valores); 
+	void CopiaValores (const MatrizDispersa &);
 public: 
 	MatrizDispersa() ; 
 	MatrizDispersa(int* filas, int* columnas, double* numeros) ; 
+	MatrizDispersa (const MatrizDispersa&);
 	~MatrizDispersa(){
 		delete[] valores;
 	}
@@ -23,9 +27,22 @@ public:
 	int getnumeroValores() const {
 		return numeroValores ; 
 	}
+	int getFilas () const {
+		return nfilas;
+	}
+	int getColumnas () const {
+		return ncolumnas;
+	}
 
-
-
+// Faltaría un método para asignar valores
+	
+	MatrizDispersa &operator= (const MatrizDispersa&);
+	
+	friend ostream& operator<< (ostream&, const MatrizDispersa&);
+	friend istream& operator>> (istream&, MatrizDispersa&);
 }
+
+ostream& operator<< (ostream&, const MatrizDispersa&);
+istream& operator>> (istream&, MatrizDispersa&);
 
 #endif 
